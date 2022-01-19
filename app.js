e69a57131e1477a68521a1e3c8dbfec979c32ab1
Thushar Prasanth <thusharprasanth@gmail.com>
2022-01-19 19:35:24 +0530

    initial settings

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const express = require("express")
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000,()=>{
+    console.log("listening to port 3000");
+})
+
+app.use('/',(req,res)=>{
+    res.render("home")
+})
