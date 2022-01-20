34975cd18fbc685fbf086472296fb986f321852f
Thushar Prasanth <thusharprasanth@gmail.com>
2022-01-20 14:42:57 +0530

    accessing datas from db

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const Blog = require('./models/blog')
+
 const app = express()
+
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended: true}));
 
-app.listen(3000,()=>{
-    console.log("listening to port 3000");
-})
+const dburl = "mongodb+srv://thusharprasanth:test1234@cluster0.vb5wu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(dburl).then(
+    (data)=>{
+        app.listen(3000,()=>{
+            console.log("listening to port 3000");
+        })
+}
+).catch((err)=>console.log(err))
 
 app.get('/',(req,res)=>{
-    res.render("home")
+    res.redirect("/blogs")
 })
 app.get('/about',(req,res)=>{
     res.render("about")
 })
+
+app.get('/blogs', (req,res)=>{
+    Blog.find()
+    .then((data)=>{
+        res.render('home',{'blogs':data})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
 app.get('/newBlog',(req,res)=>{
     res.render("newBlog")
 })
