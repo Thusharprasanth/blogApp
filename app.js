1aa5dbb21f21f825154673e718beb5d7a2b879d2
Thushar Prasanth <thusharprasanth@gmail.com>
2022-01-20 11:45:19 +0530

    adding blogs

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended: true}));
+
 app.listen(3000,()=>{
     console.log("listening to port 3000");
 })
 
-app.use('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render("home")
+})
+app.get('/about',(req,res)=>{
+    res.render("about")
+})
+app.get('/newBlog',(req,res)=>{
+    res.render("newBlog")
+})
+app.post('/newBlog',(req,res)=>{
+    const title = req.body.title;
+    const content = req.body.content
+    res.render("home", { title, content })
 })
